5c3cc1f46cfa86f3eca9ee0c7f64761b22380e26
Jack Bennett <jackbennett0708@gmail.com>
2023-10-21 14:01:34 +0100

    Set up CMake project

---
+++ src/init/main.c
@@ -0,0 +1,14 @@
+/*
+ *   Copyright (c) 2023 Jack Bennett.
+ *   All Rights Reserved.
+ *
+ *   See the LICENCE file for more information.
+ */
+
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    printf("Hello awm\n");
+
+    return 0;
+}
