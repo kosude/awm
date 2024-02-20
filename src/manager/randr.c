bb75cbb54a99188cf50d0649cf2337f86ae6c617
Jack Bennett <jackbennett0708@gmail.com>
2024-02-20 18:31:56 +0000

    RandR refactorings and organise existing code as for RandR 1.4

--- src/manager/randr.c
+++ src/manager/randr.c
@@ -12,6 +12,24 @@
 
 #include "manager/session.h"
 
+#include <string.h>
+
+// if RandR >= 1.5 is supported by the system or not
+static uint8_t has_randr_1_5 = 0;
+
+static xcb_randr_output_t *randr_find_outputs_1_4(
+    xcb_connection_t *const con,
+    const xcb_window_t root,
+    uint32_t *const len,
+    xcb_timestamp_t *const tstamp
+);
+static xcb_randr_output_t *randr_find_outputs_1_5(
+    xcb_connection_t *const con,
+    const xcb_window_t root,
+    uint32_t *const len,
+    xcb_timestamp_t *const tstamp
+);
+
 static uint8_t validate_output(
     xcb_connection_t *const con,
     const xcb_randr_output_t output,
@@ -21,36 +39,42 @@ static uint8_t validate_output(
 uint8_t randr_init(xcb_connection_t *const con, const xcb_window_t root) {
     xcb_generic_error_t *err;
 
-    uint32_t vmaj, vmin;
     uint8_t randrbase;
+
+    // queyr randr presence and retrieve randr base event
+    const xcb_query_extension_reply_t *randr = xcb_get_extension_data(con, &xcb_randr_id);
+    if (!randr->present) {
+        LFATAL("RandR is not present");
+        KILL();
+    }
+    randrbase = randr->first_event;
+
+    // check for minimum randr version 1.5
+    // TODO: configuration option to force RandR < 1.4
+    xcb_randr_query_version_reply_t *v = xcb_randr_query_version_reply(con,
+        xcb_randr_query_version(con, XCB_RANDR_MAJOR_VERSION, XCB_RANDR_MINOR_VERSION), &err);
+    if (err) {
+        LFATAL("Failed to query RandR version: %s", xerrcode_str(err->error_code));
+        free(err);
+        KILL();
+    }
+    has_randr_1_5 = (v->major_version >= 1) && (v->minor_version >= 5);
+    LINFO("Found RandR %u.%u %s", v->major_version, v->minor_version, (has_randr_1_5) ? "(>=1.5)" : "<1.5");
+    free(v);
 
     // request to recieve randr events
     xcb_void_cookie_t c = xcb_randr_select_input_checked(
         con, root,
-        (uint16_t) (XCB_RANDR_NOTIFY_MASK_CRTC_CHANGE | XCB_RANDR_NOTIFY_MASK_SCREEN_CHANGE | XCB_RANDR_NOTIFY_MASK_OUTPUT_CHANGE));
-    xcb_flush(con);
-
+        (uint16_t) (
+            XCB_RANDR_NOTIFY_MASK_CRTC_CHANGE |
+            XCB_RANDR_NOTIFY_MASK_SCREEN_CHANGE |
+            XCB_RANDR_NOTIFY_MASK_OUTPUT_CHANGE));
     if ((err = xcb_request_check(con, c))) {
         LFATAL("Failed to initialise RandR extension");
-
         free(err);
         KILL();
     }
-
-    // check for minimum randr version 1.2
-    xcb_randr_query_version_reply_t *v = xcb_randr_query_version_reply(con, xcb_randr_query_version(con, 1, 2), NULL);
-    vmaj = v->major_version, vmin = v->minor_version;
-    free(v);
-    if (vmaj <= 1 && vmin < 2) {
-        LFATAL("Missing RandR extension of at least version 1.2");
-
-        KILL();
-    }
-    LINFO("Found RandR %u.%u", vmaj, vmin);
-
-    // get randr base
-    const xcb_query_extension_reply_t *randr = xcb_get_extension_data(con, &xcb_randr_id);
-    randrbase = randr->first_event;
+    xcb_flush(con);
 
     return randrbase;
 }
@@ -74,7 +98,49 @@ out_unhandled:
 
 xcb_randr_output_t *randr_find_outputs(xcb_connection_t *const con, const xcb_window_t root, uint32_t *const len, xcb_timestamp_t *const tstamp) {
     xcb_randr_output_t *outputs, *valoutputs; // valoutputs is an array of validated outputs
-    int outputn, valoutputn;
+    uint32_t outputn, valoutputn = 0;
+    xcb_timestamp_t stamp;
+
+    if (0) {//has_randr_1_5) {
+        // find outputs with RandR >= 1.5
+        LINFO("Querying outputs with RandR 1.5");
+        outputs = randr_find_outputs_1_5(con, root, &outputn, &stamp);
+    } else {
+        // find outputs with RandR <= 1.4
+        LINFO("Querying outputs with RandR <= 1.4");
+        outputs = randr_find_outputs_1_4(con, root, &outputn, &stamp);
+    }
+
+    if (tstamp) {
+        *tstamp = stamp;
+    }
+
+    // validate each output, copying usable ones into valoutputs
+    // valoutputs will probably be bigger than necessary, but that's at least more efficient than a bunch of reallocs in the for loop
+    valoutputs = malloc(sizeof(xcb_randr_output_t) * outputn);
+    if (!valoutputs) {
+        free(outputs);
+        LFATAL("malloc() fault");
+        KILL();
+    }
+    for (uint32_t i = 0; i < outputn; i++) {
+        if (validate_output(con, outputs[i], stamp)) {
+            valoutputs[valoutputn++] = outputs[i];
+        }
+    }
+    free(outputs);
+
+    if (len) {
+        *len = valoutputn;
+    }
+
+    return valoutputs;
+}
+
+static xcb_randr_output_t *randr_find_outputs_1_4(xcb_connection_t *const con, const xcb_window_t root, uint32_t *const len,
+    xcb_timestamp_t *const tstamp) {
+    xcb_randr_output_t *outputs;
+    int32_t outputn;
 
     xcb_randr_get_screen_resources_reply_t *res = xcb_randr_get_screen_resources_reply(con,
         xcb_randr_get_screen_resources(con, root), NULL);
@@ -82,36 +148,31 @@ xcb_randr_output_t *randr_find_outputs(xcb_connection_t *const con, const xcb_wi
         return NULL;
     }
 
-    // get consistent information from the server
-    const xcb_timestamp_t stamp = res->config_timestamp;
+    // to get consistent information from the server
     if (tstamp) {
-        *tstamp = stamp;
+        *tstamp = res->config_timestamp;
     }
 
     outputn = xcb_randr_get_screen_resources_outputs_length(res);
-    outputs = xcb_randr_get_screen_resources_outputs(res);
-
-    // validate each output, copying usable ones into valoutputs
-    // valoutputs will probably be bigger than necessary, but that's at least more efficient than a bunch of reallocs in the for loop
-    valoutputs = malloc(sizeof(xcb_randr_output_t) * outputn);
-    if (!valoutputs) {
-        LFATAL("malloc() fault");
-        KILL();
-    }
-    valoutputn = 0;
-    for (int32_t i = 0; i < outputn; i++) {
-        if (validate_output(con, outputs[i], stamp)) {
-            valoutputs[valoutputn++] = outputs[i];
-        }
-    }
+    outputs = malloc(sizeof(xcb_randr_output_t) * outputn);
+    memcpy(outputs, xcb_randr_get_screen_resources_outputs(res), sizeof(xcb_randr_output_t) * outputn);
 
     if (len) {
-        *len = valoutputn;
+        *len = outputn;
     }
 
     free(res);
 
-    return valoutputs;
+    return outputs;
+}
+
+static xcb_randr_output_t *randr_find_outputs_1_5(xcb_connection_t *const con, const xcb_window_t root, uint32_t *const len,
+    xcb_timestamp_t *const tstamp) {
+    // xcb_randr_output_t *outputs;
+    // int32_t outputn;
+
+    // NOT_IMPLEMENTED
+    return NULL;
 }
 
 static uint8_t validate_output(xcb_connection_t *const con, const xcb_randr_output_t output, const xcb_timestamp_t tstamp) {
