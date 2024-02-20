3b396e19c2da663509d44a9bfa098ba03f59b4d9
Jack Bennett <jackbennett0708@gmail.com>
2024-02-20 21:43:28 +0000

    RandR 1.5 monitor retrieval

--- src/manager/randr.c
+++ src/manager/randr.c
@@ -13,6 +13,7 @@
 #include "manager/session.h"
 
 #include <string.h>
+#include <stdio.h>
 
 // if RandR >= 1.5 is supported by the system or not
 static uint8_t has_randr_1_5 = 0;
@@ -101,13 +102,19 @@ xcb_randr_output_t *randr_find_outputs(xcb_connection_t *const con, const xcb_wi
     uint32_t outputn, valoutputn = 0;
     xcb_timestamp_t stamp;
 
-    if (0) {//has_randr_1_5) {
+    if (has_randr_1_5) {
         // find outputs with RandR >= 1.5
-        LINFO("Querying outputs with RandR 1.5");
+        LINFO("Querying outputs with RandR 1.5...");
         outputs = randr_find_outputs_1_5(con, root, &outputn, &stamp);
+        if (!outputs) {
+            // 1.5 function failed, fallback to 1.4
+            LWARN("RandR 1.5 query failed, falling back to 1.4.");
+            goto fallback_1_4;
+        }
     } else {
+fallback_1_4:
         // find outputs with RandR <= 1.4
-        LINFO("Querying outputs with RandR <= 1.4");
+        LINFO("Querying outputs with RandR <= 1.4...");
         outputs = randr_find_outputs_1_4(con, root, &outputn, &stamp);
     }
 
@@ -157,22 +164,71 @@ static xcb_randr_output_t *randr_find_outputs_1_4(xcb_connection_t *const con, c
     outputs = malloc(sizeof(xcb_randr_output_t) * outputn);
     memcpy(outputs, xcb_randr_get_screen_resources_current_outputs(res), sizeof(xcb_randr_output_t) * outputn);
 
+    free(res);
+
     if (len) {
         *len = outputn;
     }
-
-    free(res);
 
     return outputs;
 }
 
 static xcb_randr_output_t *randr_find_outputs_1_5(xcb_connection_t *const con, const xcb_window_t root, uint32_t *const len,
     xcb_timestamp_t *const tstamp) {
-    // xcb_randr_output_t *outputs;
-    // int32_t outputn;
+    xcb_generic_error_t *err;
+    xcb_randr_output_t *outputs = NULL;
+    int32_t outputn = 0;
 
-    // NOT_IMPLEMENTED
-    return NULL;
+    xcb_randr_get_monitors_reply_t *mons = xcb_randr_get_monitors_reply(con, xcb_randr_get_monitors(con, root, 1), &err);
+    if (err) {
+        LERR("Failed to get RandR monitors: %s", xerrcode_str(err->error_code));
+        free(err);
+        return NULL;
+    }
+
+    LINFO("%d RandR monitors found", xcb_randr_get_monitors_monitors_length(mons));
+
+    // to get consistent information from the server
+    if (tstamp) {
+        *tstamp = mons->timestamp;
+    }
+
+    xcb_randr_monitor_info_iterator_t iter;
+    for (iter = xcb_randr_get_monitors_monitors_iterator(mons); iter.rem; xcb_randr_monitor_info_next(&iter)) {
+        const xcb_randr_monitor_info_t *info = iter.data;
+
+        const int32_t on = xcb_randr_monitor_info_outputs_length(info);
+        const xcb_randr_output_t *const ov = xcb_randr_monitor_info_outputs(info);
+
+        // concatenate the output array ov onto block at outputs
+        outputn += on;
+        outputs = realloc(outputs, sizeof(xcb_randr_output_t) * outputn);
+        memcpy(outputs + (outputn - on), ov, sizeof(xcb_randr_output_t) * on);
+
+        // // get the monitor name
+        // xcb_get_atom_name_reply_t *atomr = xcb_get_atom_name_reply(con, xcb_get_atom_name(con, info->name), &err);
+        // if (err) {
+        //     LERR("Could not get RandR monitor name: %s", xerrcode_str(err->error_code));
+        //     free(err);
+        //     continue;
+        // }
+        // // get name as string
+        // char *name;
+        // asprintf(&name, "%s", xcb_get_atom_name_name(atomr));
+        // free(atomr);
+
+        // LLOG("%s", name);
+
+        // free(name);
+    }
+
+    free(mons);
+
+    if (len) {
+        *len = outputn;
+    }
+
+    return outputs;
 }
 
 static uint8_t validate_output(xcb_connection_t *const con, const xcb_randr_output_t output, const xcb_timestamp_t tstamp) {
