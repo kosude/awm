0013ec6029bdab15609107c61ad9a1769d261b4b
Jack Bennett <jackbennett0708@gmail.com>
2024-06-22 21:41:52 +0100

    Reset source directory

---
+++ src/main.c
@@ -0,0 +1,14 @@
+/*
+ *   Copyright (c) 2024 Jack Bennett.
+ *   All Rights Reserved.
+ *
+ *   See the LICENCE file for more information.
+ */
+
+#include <stdio.h>
+
+int main(void) {
+    printf("Hello world\n");
+
+    return 0;
+}
